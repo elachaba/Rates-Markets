03b73a7b291d0312b6b46d3a445f0b5cfefcdfec
elachaba <elachabaymane995@gmail.com>
2025-02-04 13:40:55 +0100

    Fix: remove unnecessary parameters, add past matrix of hisotric prices to simulate paths

--- skeleton/src/models/GlobalModel.hpp
+++ skeleton/src/models/GlobalModel.hpp
@@ -6,25 +6,22 @@
 #define GLOBALMODEL_HPP
 
 #include <vector>
-#include <string>
-#include <nlohmann/json.hpp>
-#include "pnl/pnl_random.h"
+#include "nlohmann/json.hpp"
+#include <pnl/pnl_random.h>
 #include "Currency.hpp"
 #include "utils/TimeGrid.hpp"
 #include "financial/InterestRateModel.hpp"
 #include "financial/RiskyAsset.hpp"
 
 
-class GlobalModel {
+class GlobalModel
+{
 private:
     std::vector<Currency*> currencies_;
     std::vector<RiskyAsset*> riskyAssets_;
     ITimeGrid* monitoringTimeGrid_;
     InterestRateModel* domesticInterestRate_;
-    PnlMat *correlationMatrix_;
-    int nbAssets_;
-    int nbCurrencies_;
-    std::string domesticCurrencyId_;
+    PnlMat* correlationMatrix_;
 
 public:
     GlobalModel(const nlohmann::json& jsonParams);
@@ -32,51 +29,29 @@ public:
     virtual ~GlobalModel();
 
     /**
-     * @brief Simulate asset paths
+     * @brief Simulate assets' paths
      * @param t Current time
      * @param path Matrix to store simulated paths
+     * @param past Matrix of historic prices
      * @param rng Random number generator
      */
-    void simulate(double t,  PnlMat* path, PnlRng* rng);
+    void simulate(double t, PnlMat* path, const PnlMat* past, PnlRng* rng);
 
     /**
-     * @brief Simulate with shifted asset
+     * @brief Simulated with shifted asset
      * @param t Current time
      * @param path Matrix to store simulated paths
-     * @param assetIndex Index of asset to shift
+     * @param past Matrix of the historic prices
      * @param h Size of the shift
-     * @param rng Random number generator
+     * @rng Random number generator
      */
-    void shiftAsset(double t, PnlMat* path, int assetIndex, double h, PnlRng* rng);
+    void shiftAsset(double t, PnlMat* path, const PnlMat* past, double h, PnlRng* rng);
 
     /**
-     * @brief Get total number oof assets (including currencies)
+     * @brief Initialize the model from JSON parameters
      */
-    int getTotalSize() const
-    {
-        return nbAssets_ + nbCurrencies_;
-    }
 
-    /**
-     * @brief Get the monitoring dates
-     */
-    const ITimeGrid* getTimeGrid() const
-    {
-        return monitoringTimeGrid_;
-    }
-
-private:
-    /**
-     * @brief Initialize model from JSON parameters
-     */
     void initFromJson(const nlohmann::json& jsonParams);
-
-    /**
-     * @brief Apply Cholesky correlation to random vector
-     */
-    void applyCorrelation(PnlVect* randomVector);
 };
-
-
 
 #endif //GLOBALMODEL_HPP
