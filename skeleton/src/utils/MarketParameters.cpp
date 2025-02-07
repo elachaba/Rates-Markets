9782fc44af1908d0606fa0aa501354d2e9c10e6d
elachaba <elachabaymane995@gmail.com>
2025-02-07 12:43:13 +0100

    Feat: implement market data to store the assets's info and currencies' info

--- skeleton/src/utils/MarketParameters.cpp
+++ skeleton/src/utils/MarketParameters.cpp
@@ -1,5 +1,71 @@
 //
-// Created by ASUS on 06/02/2025.
+// Created by Aymane on 06/02/2025.
 //
-
+#include "json_reader.hpp"
 #include "MarketParameters.hpp"
+
+MarketParameters MarketParameters::fromJson(const nlohmann::json& j)
+{
+    MarketParameters marketParams;
+
+    marketParams.domesticCurrencyId = j.at("DomesticCurrencyId").get<std::string>();
+
+    double domesticInterestRate = 0.0;
+
+    for (const auto& currency : j.at("Currencies"))
+    {
+        if (currency.at("id") == marketParams.domesticCurrencyId)
+            domesticInterestRate = currency.at("InterestRate").get<double>();
+    }
+
+    for (const auto& currency : j.at("Currencies"))
+    {
+        CurrencyInfo info{};
+        info.id = currency.at("id").get<std::string>();
+        info.interestRate = currency.at("InterestRate").get<double>();
+        info.volatility = currency.at("Volatility").get<double>();
+        if (info.id != marketParams.domesticCurrencyId)
+            info.drift = domesticInterestRate;
+        else
+            info.drift = 0.0;
+
+        marketParams.currencies.push_back(info);
+    }
+
+    // Parse assets
+    for (const auto& asset : j.at("Assets"))
+    {
+        AssetInfo info{};
+        info.currencyId = asset.at("CurrencyId").get<std::string>();
+        info.drift = domesticInterestRate;
+        info.volatility = asset.at("Volatility").get<double>();
+
+        marketParams.assets.push_back(info);
+    }
+
+    // Parse correlation matrix
+    marketParams.correlationMatrix = nullptr;
+    j.at("Correlations").get_to(marketParams.correlationMatrix);
+
+    return marketParams;
+
+}
+
+double MarketParameters::getDomesticRate() const
+{
+    for (const auto& currency : MarketParameters::currencies)
+    {
+        if (currency.id == domesticCurrencyId)
+            return currency.interestRate;
+    }
+}
+
+MarketParameters::~MarketParameters()
+{
+    if (correlationMatrix)
+    {
+        pnl_mat_free(&correlationMatrix);
+        correlationMatrix = nullptr;
+    }
+}
+
--- skeleton/src/utils/MarketParameters.hpp
+++ skeleton/src/utils/MarketParameters.hpp
@@ -1,13 +1,55 @@
 //
-// Created by ASUS on 06/02/2025.
+// Created by A on 0ymane6/02/2025.
 //
 
 #ifndef MARKETPARAMETERS_HPP
 #define MARKETPARAMETERS_HPP
-
+#include <optional>
+#include <string>
+#include <vector>
+#include <nlohmann/adl_serializer.hpp>
+#include <pnl/pnl_matrix.h>
 
 
 class MarketParameters {
+public:
+    struct CurrencyInfo
+    {
+        std::string id;
+        double interestRate;
+        double volatility;
+        double drift;  // Make drift optional
+
+    };
+
+    struct AssetInfo
+    {
+        std::string currencyId;
+        double drift;
+        double volatility;
+    };
+
+    std::string domesticCurrencyId;
+    std::vector<CurrencyInfo> currencies;
+    std::vector<AssetInfo> assets;
+    PnlMat* correlationMatrix;
+
+    /**
+     * @brief Creates MarketParameters from JSON
+     * @param j JSON containing market parameters
+     * @return MarketParameters object
+     * @throw std::runtime_error if parameters are invalid
+     */
+    static MarketParameters fromJson(const nlohmann::json& j);
+
+
+    /**
+     * @brief Get domestic interest rate
+     * @return Domestic interest rate
+     */
+    double getDomesticRate() const;
+
+    ~MarketParameters();
 
 };
 
