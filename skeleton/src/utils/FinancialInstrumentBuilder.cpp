59833c484b0f28bb234cc599273a2479141217b0
elachaba <elachabaymane995@gmail.com>
2025-02-07 12:41:12 +0100

    Feat: implement financial instrument builder from JSON file

--- skeleton/src/utils/FinancialInstrumentBuilder.cpp
+++ skeleton/src/utils/FinancialInstrumentBuilder.cpp
@@ -1,5 +1,73 @@
 //
-// Created by ASUS on 06/02/2025.
+// Created by Aymane on 06/02/2025.
 //
 
 #include "FinancialInstrumentBuilder.hpp"
+
+std::vector<int> FinancialInstrumentBuilder::buildAssetCorrelationMapping(const MarketParameters& params)
+{
+    std::map<std::string, int> currencyToIndex;
+    currencyToIndex[params.domesticCurrencyId] = 0;
+
+    int currencyIndex = 1;
+    for (const auto& currency : params.currencies)
+    {
+        if (currency.id != params.domesticCurrencyId)
+            currencyToIndex[currency.id] = currencyIndex++;
+    }
+
+    std::vector<int> mapping;
+    mapping.reserve(params.assets.size());
+
+    for (const auto& asset: params.assets)
+    {
+        int idx = currencyToIndex.at(asset.currencyId);
+        mapping.push_back(idx);
+    }
+
+    return mapping;
+}
+
+std::pair<std::vector<InterestRateModel*>, InterestRateModel*>
+    FinancialInstrumentBuilder::buildInterestRateModels(const MarketParameters& params)
+{
+    std::vector<InterestRateModel*> foreignRates;
+    InterestRateModel* domesticRate = nullptr;
+
+    for (const auto& currency : params.currencies)
+    {
+        if (currency.id == params.domesticCurrencyId)
+            domesticRate = new InterestRateModel(currency.interestRate);
+        else
+            foreignRates.push_back(new InterestRateModel(currency.interestRate));
+    }
+
+    if (!domesticRate)
+        throw std::runtime_error(
+            "FinancialInstrumentBuilder::buildInterestRateModels: Domestic interest rate not found"
+            );
+
+    return {foreignRates, domesticRate};
+}
+
+PnlMat* FinancialInstrumentBuilder::createCholeskyMatrix(const PnlMat* correlationMatrix)
+{
+    if (!correlationMatrix)
+        throw std::runtime_error(
+            "FinancialInstrumentBuilder::createCholeskyMatrix: Correlation matrix is null"
+            );
+
+    PnlMat* cholesky = pnl_mat_copy(correlationMatrix);
+
+    if (pnl_mat_chol(cholesky) != 0)
+    {
+        pnl_mat_free(&cholesky);
+        throw std::runtime_error(
+            "FinancialInstrumentBuilder::createCholeskyMatrix: Cholesky decomposition failed"
+            );
+    }
+
+    return cholesky;
+}
+
+
--- skeleton/src/utils/FinancialInstrumentBuilder.hpp
+++ skeleton/src/utils/FinancialInstrumentBuilder.hpp
@@ -1,13 +1,42 @@
 //
-// Created by ASUS on 06/02/2025.
+// Created by Aymane on 06/02/2025.
 //
 
 #ifndef FINANCIALINSTRUMENTBUILDER_HPP
 #define FINANCIALINSTRUMENTBUILDER_HPP
 
+#include <vector>
+
+#include "MarketParameters.hpp"
+#include "financial/InterestRateModel.hpp"
 
 
 class FinancialInstrumentBuilder {
+public:
+
+    /**
+     * @brief Build mapping between assets and their currencies
+     * @param params Market parameters
+     * @return Vector where index i contains the currency index for asset i
+     */
+    static std::vector<int> buildAssetCorrelationMapping(const MarketParameters& params);
+
+    /**
+     * @brief Build interest rate models for all currencies
+     * @param params Market parameters
+     * @return Pair of (foreign rates vector, domestic rate)
+     */
+    static std::pair<std::vector<InterestRateModel*>, InterestRateModel*>
+        buildInterestRateModels(const MarketParameters& params);
+
+
+    /**
+     * @brief Create Cholesky decomposition of correlation matrix
+     * @param correlationMatrix Original correlation matrix
+     * @return Cholesky decomposed matrix
+     * @throw std::runtime_error if decomposition fails
+     */
+    static PnlMat* createCholeskyMatrix(const PnlMat* correlationMatrix);
 
 };
 
